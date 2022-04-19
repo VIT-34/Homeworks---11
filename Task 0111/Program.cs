141a39b37bead05a4be0784b6f2eca92baf5ee2e
Vitaliy <viurum986@mail.ru>
2022-04-19 21:23:17 +0300

    Task 0111

---
+++ Task 0111/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information  
+
+ 
+class Program
+{
+    static void Main(string[] args)
+    {Console.WriteLine("введите число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        
+        if (step(a)) Console.WriteLine($"число {a} является степень двойки");
+        else Console.WriteLine($"число {a} не является степенью двойки");
+        Console.WriteLine();
+    }
+    public static bool step(int a)
+    {
+        if (a == 2) return true;
+        else if (a % 2 == 0) return step(a / 2);
+        else return false;
+    }
+}
